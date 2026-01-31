58afabee81b4b239a4a1350ffd1f5630f45a313b
bjw446 <bjw446@naver.com>
2026-01-31 14:28:27 +0900

    Initial commit

---
+++ src/main/java/com/example/cloudarchitecturetask/CloudArchitectureTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudarchitecturetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudArchitectureTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudArchitectureTaskApplication.class, args);
+    }
+
+}
